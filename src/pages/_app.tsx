edce864ab8b12addb9ff07f2aae6a5b5adae53fc
PiotrK <piotrek380@gmail.com>
2023-03-01 17:57:35 +0100

    Add a navigation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
 import "@/styles/globals.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 const client = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
-      <Component {...pageProps} />
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/fetchuser">Fetch Single User</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
     </QueryClientProvider>
   );
 }
---
+++ src/pages/fetchuser.tsx
@@ -0,0 +1,5 @@
+const FetchUserPage = () => {
+  return <h1>Fetch User Page</h1>;
+};
+
+export default FetchUserPage;
